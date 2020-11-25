564bb3f34c2787cb82099326e2e1f60d354f6048
ChumpyTheBear <ppizzimenti@gmail.com>
2020-11-25 14:31:56 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -20,6 +20,7 @@ ufw default deny outgoing
 ufw default deny incoming
 ufw allow out on tun0 from any to any
 ufw allow out on eth0 to any port 1194 proto udp
+ufw allow out on eth0 to any port 1443 proto tcp
 ufw allow out on eth0 to 1.1.1.1 port 53 proto udp
 ufw allow in on eth0 from ${LAN_NETWORK} to any
 ufw enable
