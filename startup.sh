d320dcfe7ac95fbe8a7bffa68cb580b035a3f989
ChumpyTheBear <ppizzimenti@gmail.com>
2020-11-27 18:14:26 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -29,8 +29,7 @@ fi
 
 if [ -n ${ENABLE_DOCKERNETWORK}  ]
 then
-    ufw allow in on eth0 from 172.17.0.0/16 to any
-    ufw allow in on eth0 from 172.18.0.0/16 to any
+    ufw allow in on eth0 from 172.0.0.0/8 to any
     echo Added firewall rule to allow incoming connections from default internal Docker networks
 fi
 
